80deddb6e21ae2fbf067d9bb3ea052cd94498951
shameer125 <alishameer251@gmail.com>
2026-04-13 22:49:47 -0700

    Adding Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="bg-blue-900 text-white px-6 py-16 grid md:grid-cols-2 gap-10 items-center">
+      <div>
+        <h1 className="text-4xl md:text-5xl font-bold mb-6">
+          Get More Done with whitepace
+        </h1>
+        <p className="mb-6 text-gray-200">
+          Project management software that enables your teams to collaborate.
+        </p>
+        <button className="bg-blue-500 px-6 py-3 rounded-xl">
+          Try whitepace free
+        </button>
+      </div>
+      <div className="bg-white h-64 rounded-xl"></div>
+    </section>
+  );
+}
+
+export default Hero
